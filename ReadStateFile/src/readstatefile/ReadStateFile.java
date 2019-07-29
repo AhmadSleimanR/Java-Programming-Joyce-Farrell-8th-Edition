c185c103891e74df4ebb88cde5e33fd87de69e04
Darklycan51 <werty_51@hotmail.com>
2019-07-29 10:20:34 -0500

    Added two new exercises
    This commit contains two new exercises based on nio and io

---
+++ ReadStateFile/src/readstatefile/ReadStateFile.java
@@ -0,0 +1,42 @@
+package readstatefile;
+import java.nio.file.*;
+import java.io.*;
+import static java.nio.file.StandardOpenOption.*;
+import java.nio.file.attribute.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Scanner;
+/**
+ *
+ * @author lycan
+ */
+public class ReadStateFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String fileName;
+        System.out.print("Enter name of file to use: ");
+        fileName = scan.nextLine();
+        fileName = "E:\\testFiles\\"+fileName;
+        Path file = Paths.get(fileName);
+        final String ID_FORMAT = "000";
+        final String NAME_FORMAT = "       ";
+        final int NAME_LENGTH = NAME_FORMAT.length();
+        final String HOME_STATE = "WI";
+        final String BALANCE_FORMAT = "0000.00";
+        String delimiter = ",";
+        String s = ID_FORMAT + delimiter + NAME_FORMAT + delimiter
+                + HOME_STATE + delimiter + BALANCE_FORMAT 
+                + System.getProperty("line.separator");
+        final int RECSIZE = s.length();
+        byte data[] = s.getBytes();
+        final String EMPTY_ACCT = "000";
+        String[] array = new String[4];
+        double balance;
+        double total = 0;
+    }
+}
